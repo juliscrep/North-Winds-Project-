d7baf7e05ad46bbeb6918c574e11bebbc26cc5b2
juliscrep <julietascrepnik@gmail.com>
2024-01-29 13:35:11 -0300

    cambios en codigo

--- components/objectives.jsx
+++ components/objectives.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/objectives.module.css';
+import styles from '../Styles/objectives.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
--- components/servicesList.jsx
+++ components/servicesList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/servicesList.module.css";
+import styles from "../Styles/servicesList.module.css";
 import Image from 'next/image';
 import Link from 'next/link';
 
