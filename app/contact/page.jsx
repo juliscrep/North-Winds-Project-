daf430751f600828c3853a2f4c27da482b15ddb4
juliscrep <julietascrepnik@gmail.com>
2024-02-12 20:26:07 -0300

    mejoras al codigo

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -14,20 +14,25 @@ function ContactPage(){
              <div className={styles.contactSection} >
                 <i className={`fa fa-home ${styles.icon}`}></i>
             
-            <h6 className={styles.iconBoxTitle2}>RN60 y Av. 24 de Mayo, Aimogasta, La Rioja, Argentina (CP 5310)</h6>
-            <h6 className={styles.iconBoxTitle4}>Horario de atención: Lunes a viernes de 8:00hs a 12:00hs y de 14:00hs a 18:00hs</h6>
-                     
+                <span className={styles.iconBoxTitle2}>RN60 y Av. 24 de Mayo, Aimogasta, La Rioja, Argentina (CP 5310)</span>
+                               
              </div>
             
+             <div className={styles.contactSection} >
+                <i className={`fa fa-clock ${styles.icon}`}></i>
+            
+               <span className={styles.iconBoxTitle4}>Horario de atención: Lunes a viernes de 8:00hs a 12:00hs y de 14:00hs a 18:00hs</span>
+                     
+             </div>
+
             <div className={styles.contactSection} >
                 <i className="fa fa-phone"></i>
-                <span className={styles.iconBoxTitle}>(+54) 380 4617560</span>
-                <h6 className={styles.iconBoxTitle3}> (+54) 3827 453714</h6>
+                <span className={styles.iconBoxTitle2}>(+54) 380 4617560 / (+54) 3827 453714</span>
             </div>                
         
             <div className={styles.contactSection}>
                 <i className="fa fa-envelope"></i>
-                <span className={styles.iconBoxTitle}>northwinds1223@gmail.com</span>
+                <span className={styles.iconBoxTitle2}>northwinds1223@gmail.com</span>
             </div>        
         </div>
         <Form></Form>
